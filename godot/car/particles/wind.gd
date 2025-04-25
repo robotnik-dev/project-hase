975b091858691bbafaf63cb00794b916fd4caa60
robotnik-dev <robotnik.games@gmail.com>
2025-04-25 17:10:58 +0200

    feat: add wind particle effects to vehicles
    - Updated cabrio, car, truck scenes to include wind particle effects.
    - Introduced wind.gd script for controlling particle emission based on vehicle speed.

---
+++ godot/car/particles/wind.gd
@@ -0,0 +1,13 @@
+extends GPUParticles3D
+
+@export var on_speed_threshold: float = 30.0
+@export var off_speed_threshold: float = 10.0
+
+@onready var car: Car = get_parent()
+
+func _physics_process(_delta: float) -> void:
+	if car.linear_velocity.z > on_speed_threshold:
+		emitting = true
+	# hystherese effect
+	elif car.linear_velocity.z < off_speed_threshold:
+		emitting = false
