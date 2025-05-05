391aa32eef852d3b954694fa158e9de58fe8fd8e
robotnik-dev <robotnik.games@gmail.com>
2025-05-05 13:53:35 +0200

    feat: implement grave pooling and management system
    - Added a grave pool to manage instantiated graves.
    - Updated grave placement logic to reuse graves.
    - Enhanced grave setup with position and angle adjustments.
    - Introduced signals for boost button interactions.

--- godot/graves/grave.gd
+++ godot/graves/grave.gd
@@ -1,9 +1,18 @@
 extends Node3D
 class_name Grave
 
+@export var width: float
+@export var height: float
+
 @onready var label = $Mesh/Label3D
+@onready var start_pos: Vector3 = position
+
+var used: bool = false
 
 func setup(spawn_position: Vector3, x_rotation: float, text: String):
 	label.text = text
-	global_position = spawn_position + position
+	global_position = spawn_position + start_pos
 	global_rotation.x = x_rotation
+
+func get_angle() -> float:
+	return global_rotation.x
--- godot/ui/buttons/boost_button.gd
+++ godot/ui/buttons/boost_button.gd
@@ -9,6 +9,9 @@ signal boost_pressed
 func _ready():
 	button.disabled = true
 	Speedboost.boost_ready.connect(_on_boost_ready)
+	Speedboost.boost_used.connect(_on_boost_used)
+	Signals.connect_car_crashed(_on_car_crashed)
+	Signals.connect_camera_loaded(_on_camera_loaded)
 
 
 func _process(_delta: float) -> void:
@@ -18,6 +21,20 @@ func _process(_delta: float) -> void:
 func _on_boost_ready():
 	button.disabled = false
 	progress.material.set("shader_parameter/enabled", true)
+
+
+func _on_car_crashed(_a,_b,_c):
+	button.disabled = true
+	progress.material.set("shader_parameter/enabled", false)
+
+
+func _on_camera_loaded():
+	button.disabled = false
+
+
+func _on_boost_used():
+	button.disabled = true
+	progress.material.set("shader_parameter/enabled", false)
 
 
 func _on_button_pressed() -> void:
