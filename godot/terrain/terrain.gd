b58b7d99cd6e6b761a993838b37486b44a71ff62
robotnik-dev <robotnik.games@gmail.com>
2025-05-08 10:55:06 +0200

    refactor: terrain node from scratch, without presets #1

--- godot/terrain/terrain.gd
+++ godot/terrain/terrain.gd
@@ -1,6 +1,43 @@
 @tool
 extends Path3D
 
+@export_tool_button("Regenerate Collision") var generate_collision_button = generate_collision
+
+@export var static_body: StaticBody3D
+@export var csg: CSGPolygon3D
+
+@export var thickness: float = 10.0:
+	set(value):
+		if !csg:
+			return
+		
+		thickness = value
+		csg .polygon[0].y = -thickness
+		csg .polygon[3].y = -thickness
+
+# TODO: enum for round or straight edges
+
+func generate_collision():
+	if !get_parent().is_editable_instance(self):
+		get_parent().set_editable_instance(self, true)
+	clear_collision()
+	regenerate_collision()
+
+
+func clear_collision():
+	for c in static_body.get_children():
+		static_body.remove_child(c)
+		c.queue_free()
+
+func regenerate_collision():
+	var collision_shape = csg.bake_collision_shape()
+	collision_shape.backface_collision = true
+	collision_shape.resource_local_to_scene = true
+	var collision = CollisionShape3D.new()
+	static_body.add_child(collision)
+	collision.shape = collision_shape
+	collision.owner = get_tree().edited_scene_root
+
 func _on_curve_changed() -> void:
 	if not curve:
 		return
@@ -14,3 +51,8 @@ func _on_curve_changed() -> void:
 			curve.set_point_in(i, Vector3(0, _in.y, _in.z))
 		if _out.x != 0:
 			curve.set_point_out(i, Vector3(0, _out.y, _out.z))
+
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		# lock rotation around y
+		global_rotation_degrees.y = 0.0
