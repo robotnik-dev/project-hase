a2a12c4c3d554119d551ac9f8b858586eca440b4
robotnik-dev <robotnik.games@gmail.com>
2025-05-16 14:28:24 +0200

    feat: implement level selection and previews for desert and jungle levels

--- godot/global/game_stats.gd
+++ godot/global/game_stats.gd
@@ -6,7 +6,7 @@ var playtime: float = 0.0:
 	get():
 		return roundf(Time.get_ticks_msec() / 1000.0 / 60.0)
 
-var cars_unlocked: int = 0:
+var cars_unlocked: int = 1:
 	get():
 		var sum_collected = Collectibles.get_sum_of_collected()
 		if sum_collected >= 6:
@@ -22,10 +22,12 @@ var max_cars: int = 3
 var collected: int = 0:
 	get():
 		return Collectibles.get_sum_of_collected()
-var max_collectibles: int = 9
+var max_collectibles: int = 19
 var level_completed: int = 0
 
 func _ready() -> void:
 	Signals.connect_car_crashed(func(_a,_b,_c): crashes += 1)
 	Signals.connect_car_flipped(func(_a,_b): flips += 1)
-	#Signals.connect_car_finished(func(): level_completed += 1)
+	# TODO: maybe differentiate between levels here but ones the first level completes,
+	# unlock all for now
+	Signals.connect_car_finished(func(): level_completed += 1)
--- godot/ui/screens/pause_menu.gd
+++ godot/ui/screens/pause_menu.gd
@@ -2,15 +2,12 @@ extends Control
 class_name UIPauseMenu
 
 @export var continue_button: Button
-@export var cars: CenterContainer
 
 func _ready() -> void:
 	Signals.connect_continue_level_button_pressed(toggle_pause)
 	Signals.connect_car_crashed(_on_car_crashed)
 	Signals.connect_camera_loaded(_on_camera_loaded)
 	visible = false
-	if GameStats.cars_unlocked > 1:
-		cars.show()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
